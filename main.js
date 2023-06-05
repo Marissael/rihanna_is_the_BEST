3dd766a5e2fe79f49c428a050bca79b51e0ad3db
Marissael <marissaelb2007@gmail.com>
2023-06-05 10:40:36 -0300

    sexto cambio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let miNombre = "Marissael"
 let miApellido = "Padilla"
 let miEdad = 16
@@ -8,14 +9,4 @@ const ESPACIO = " "
 var nombre = prompt("Por favor, ingresa tu nombre:", "Ejemplo")
 var nombree = alert("hola, " + nombre + ESPACIO + "bienvenido!!");
 
-
-
-let numero = prompt("ingrese un numero")
-if (numero > 98) {
-    alert("tu numero es mayor a 98")
-} else if (numero == 98) {
-    alert("es 98")
-} else {
-    alert("no es mayor a 98")
-}
 
