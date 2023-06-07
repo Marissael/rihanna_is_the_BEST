29c546c9d895fe797c8747f810c19cee98cda490
Marissael <marissaelb2007@gmail.com>
2023-06-07 19:05:37 -0300

    7mo cambios

--- 2main.js
+++ 2main.js
@@ -1,7 +1,8 @@
 function toggleAccordion(index) {
-    const accordionContent = document.querySelectorAll('.accordion-content');
-    const accordionButton = document.querySelectorAll('.accordion-button');
+    const accordionContent = document.querySelectorAll('.accordion-content'); /* Selecciona todos los elementos con la clase 'accordion-content' */
+    const accordionButton = document.querySelectorAll('.accordion-button'); /* Selecciona todos los elementos con la clase 'accordion-button' */
   
-    accordionContent[index].classList.toggle('show');
-    accordionButton[index].classList.toggle('collapsed');
-}
+    accordionContent[index].classList.toggle('show'); /* Agrega la clase 'show' al contenido del índex o pagina.html */
+    accordionButton[index].classList.toggle('collapsed'); /* Agrega o remueve la clase 'collapsed' al botón del índice proporcionado */
+  }
+  
--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let miMadre = "Mariarkis"
 
 const ESPACIO = " "
 
+//cuando la persona recargue o recien entre a la pagina le va a aparecer un prompt para q ingrese su nombre, y luego una alerta que diga Hola, ____ Bienvenido!
 var nombre = prompt("Por favor, ingresa tu nombre:", "Ejemplo")
 var nombree = alert("hola, " + nombre + ESPACIO + "bienvenido!!");
 
